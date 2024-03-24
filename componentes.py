16930aaeb7589669e484464e32b2b8b39371e406
felipemarianoferr <113635832+felipemarianoferr@users.noreply.github.com>
2024-03-24 01:40:20 +0000

    falta ex simu, ex4_sub 3

--- componentes.py
+++ componentes.py
@@ -18,15 +18,26 @@ def exe1(a, b, c, s):
 def exe2(l, m, h, l_vd, l_am, l_vm, l_az, l_lj):
     @always_comb
     def comb():
-        pass
+        l_vd.next = l and not m and not h
 
+        l_am.next = l and m and not h
+
+        l_vm.next = l and m and h
+
+        l_az.next =  not l and not m and not h
+            
+        l_lj.next =  (h and (not l or not m)) or (m and not l)
+            
+        
     return instances()
 
 
 def exe3(i3, i2, i1, i0, p1, p0, v):
     @always_comb
     def comb():
-        pass
+        p1.next = i3 or i2
+        p0.next = ((not i3) and (not i2) and i1) or (i3)
+        v.next = i1 or i2 or i3 or i0
 
     return instances()
 
@@ -52,7 +63,8 @@ def exe4_simulando(a, b, c, d, e, f, g, h, i, j, k, l):
 def exe4_half_sub(x, y, b, d):
     @always_comb
     def comb():
-        pass
+        b.next = (not x) and y
+        d.next = ((not x) and y) or (x and (not y))
 
     return instances()
 
@@ -60,7 +72,13 @@ def exe4_half_sub(x, y, b, d):
 def exe4_full_sub(x, y, z, b, d):
     @always_comb
     def comb():
-        pass
+        d.next = (
+            ((not x) and (not y) and z)
+            or (not x and y and (not z))
+            or ((not x) and y and z)
+            or (x and y and z)
+        )
+        b.next = ((not x) and y) or ((not x) and z) or (y and z)
 
     return instances()
 
